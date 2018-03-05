7e00681d50e5724faaf7473d2a9aeabb22b485cc
mawad8 <mawad8@yahoo.com>
2018-03-04 21:57:31 -0800

    JS&css&html
    this website for car dealer

---
+++ Cars-proj/js/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    
+
+}
+function call(){
+	var h1text = document.getElementById('bbbb');
+	var hh11 = document.createTextNode('Ph:- (408)662-9354')
+	h1text.appendChild(hh11);
+}
